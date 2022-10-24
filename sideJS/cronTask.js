5fd519de9823661c606b5099b3d7aa0c0e0d6746
TW - IT(WEB) - Lion Chuang <lion.chuang@shoalter.com>
2022-10-24 21:10:54 +0800

    fix cronTask.js

--- sideJS/cronTask.js
+++ sideJS/cronTask.js
@@ -3,12 +3,14 @@ var request = require('request');
 require('dotenv').config();
 
 // 定時呼叫自己，防止睡眠
-exports.cronCallMysell = schedule.scheduleJob('*/10 * * * *', () => {
-  const myPage = {
-    'method': 'GET',
-    'url': process.env.HOME_PAGE,
-    'headers': {}
-  };
+exports.cronCallMysell = function () {
+  schedule.scheduleJob('*/10 * * * *', () => {
+    const myPage = {
+      'method': 'GET',
+      'url': process.env.HOME_PAGE,
+      'headers': {}
+    };
 
-  request(myPage, () => { });
-})
+    request(myPage, () => { });
+  })
+};
