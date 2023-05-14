58a7a79640eefe30c09106f8c8b409ce3243849b
TW - IT(WEB) - Lion Chuang <lion.chuang@shoalter.com>
2023-05-15 00:42:07 +0800

    push

---
+++ dataBaseManager/dataBaseM.js
@@ -0,0 +1,39 @@
+const axios = require('axios');
+
+//獲取botMessage的全部資料
+exports.getDataFormBotMessage = function (callback) {
+  const config = {
+    method: 'post',
+    url: process.env.MYDATABASE_GETDATAFORMBOTMESSAGE,
+    headers: {}
+  };
+
+  axios(config)
+    .then(function (response) {
+      //callback(JSON.stringify(response.data));
+      callback(JSON.parse(JSON.stringify(response.data)));
+    })
+    .catch(function (error) {
+      console.log('ERROR#MyDataBase#01: ', error);
+      callback(undefined);
+    });
+}
+
+//獲取userMessage的全部資料
+exports.getDataFormUserMessage = function (callback) {
+  const config = {
+    method: 'post',
+    url: process.env.MYDATABASE_GETDATAFORMUSERMESSAGE,
+    headers: {}
+  };
+
+  axios(config)
+    .then(function (response) {
+      //callback(JSON.stringify(response.data));
+      callback(JSON.parse(JSON.stringify(response.data)));
+    })
+    .catch(function (error) {
+      console.log('ERROR#MyDataBase#02: ', error);
+      callback(undefined);
+    });
+}
