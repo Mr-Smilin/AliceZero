262a5b89d47e9e71154b3589e0dc8f1e4aa0e751
TW - IT(WEB) - Lion Chuang <lion.chuang@shoalter.com>
2023-05-16 15:12:30 +0800

    修改定時呼叫作業

--- sideJS/cronTask.js
+++ sideJS/cronTask.js
@@ -4,13 +4,13 @@ require('dotenv').config();
 
 // 定時呼叫自己，防止睡眠
 exports.cronCallMysell = function () {
+  const myPage = {
+    'method': 'GET',
+    'url': process.env.HOME_PAGE,
+    'headers': {}
+  };
+  request(myPage, () => { });
   schedule.scheduleJob('*/10 * * * *', () => {
-    const myPage = {
-      'method': 'GET',
-      'url': process.env.HOME_PAGE,
-      'headers': {}
-    };
-
     request(myPage, () => { });
   })
 };
