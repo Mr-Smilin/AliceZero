e4a3fb308e3a6cc94eaa64d1f384e2a44817d8a5
Smilin <lion31lion31@gmail.com>
2023-05-27 20:46:00 +0800

    自我呼叫添加header

--- baseJS/CronTask.js
+++ baseJS/CronTask.js
@@ -8,7 +8,7 @@ const axios = require("axios");
 exports.cronCallMysell = function () {
   schedule.scheduleJob('*/5 * * * *', async () => {
     try {
-      const response = await getData(process.env.HOME_PAGE);
+      const response = await getData(process.env.HOME_PAGE, "GET", configs);
       CatchF.LogDo(response, "自我呼叫 response");
     }
     catch (err) {
@@ -17,18 +17,25 @@ exports.cronCallMysell = function () {
   })
 };
 
-const selectMethod = async (url, method, body = {}) => {
+const configs = {
+  headers: {
+    "Content-Type": "application/json",
+    "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Safari/537.36"
+  },
+}
+
+const selectMethod = async (url, method, config) => {
   switch (method) {
     case "GET":
-      return await axios.get(url);
+      return await axios.get(url, config);
     case "POST":
-      return await axios.post(url, body);
+      return await axios.post(url, config);
   }
 }
 
-const getData = async (url, method = "GET", callback = async () => { }) => {
+const getData = async (url, method = "GET", config = {}, callback = async () => { }) => {
   try {
-    const response = await selectMethod(url, method);
+    const response = await selectMethod(url, method, config);
     const data = response.data;
     await callback(data);
     return data;
