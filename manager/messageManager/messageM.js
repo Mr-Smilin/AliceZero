3eff17b8412dd5d361abc319796fcb2330da71dc
2413 莊育誠 lionchuang <lionchuang@pershing.com.tw>
2023-08-30 11:12:18 +0800

    fix bug

--- manager/messageManager/messageM.js
+++ manager/messageManager/messageM.js
@@ -4,7 +4,7 @@ const BDB = require("../../baseJS/BaseDiscordBot.js");
 // js
 const CatchF = require("../../baseJS/CatchF.js");
 const MessageC = require("./messageC.js");
-const messageConstant = require("./messageConstant.js")''
+const messageConstant = require("./messageConstant.js");
 const musicM = require("../musicManager/musicM.js");
 const myKiritoM = require("../mykiritoManager/myKiritoM.js");
 const trpgM = require("../trpgManager/trpgM.js");
@@ -24,19 +24,22 @@ exports.Start = async (msg) => {
 
 	try {
 		// 掃地
-		if (BDB.MGetChannelId(msg) === '709293980684386344' || BDB.MGetChannelId(msg) === '716316365555761183' && BDB.MContent(msg).indexOf("掃地") > -1)
+		if (
+			BDB.MGetChannelId(msg) === "709293980684386344" ||
+			(BDB.MGetChannelId(msg) === "716316365555761183" &&
+				BDB.MContent(msg).indexOf("掃地") > -1)
+		)
 			nineDatas.execute(msg, nineDatas.data);
 	} catch (err) {
 		CatchF.ErrorDo(err, "掃地方法出錯啦");
 	}
 
 	try {
-		let cmd = BDB.MContent(msg).split(' ');
+		let cmd = BDB.MContent(msg).split(" ");
 		let args = [];
 
 		if (cmd[1] === undefined) {
-			if (cmd[0] !== undefined)
-				await SelectFunctionWithPrefix(msg, cmd);
+			if (cmd[0] !== undefined) await SelectFunctionWithPrefix(msg, cmd);
 		} else {
 			args = cmd.splice(2, cmd.length - 2);
 			await SelectFunctionWithPrefix(msg, cmd, args);
@@ -47,9 +50,9 @@ exports.Start = async (msg) => {
 };
 
 /**
- * 
+ *
  * @param {*} msg discord.message
- * @param {*} cmd 0 - 前綴字 1 - 指令 
+ * @param {*} cmd 0 - 前綴字 1 - 指令
  * @param {*} args 參數
  */
 async function SelectFunctionWithPrefix(msg, cmd, args = []) {
@@ -72,11 +75,10 @@ async function SelectFunctionWithPrefix(msg, cmd, args = []) {
 		nowPrefix = DeleteTempIfHaveEx(BDB.MContent(msg), nowPrefix);
 	}
 
-
 	switch (nowPrefix) {
 		// 基本方法
 		case "0":
-			MessageC.DoBaseFunction(msg, cmd[1], args)
+			MessageC.DoBaseFunction(msg, cmd[1], args);
 			break;
 		// 音樂方法
 		case "1":
@@ -97,7 +99,7 @@ async function SelectFunctionWithPrefix(msg, cmd, args = []) {
 function DeleteTempIfHaveEx(msg, temp) {
 	let tempValue = temp;
 	//if (msg.substring(0, 4) !== 'http') {
-	if (tempValue != '1') {
+	if (tempValue != "1") {
 		const t = /\@|\:/;
 		if (t.test(msg)) tempValue = -1;
 	}
@@ -106,11 +108,21 @@ function DeleteTempIfHaveEx(msg, temp) {
 
 //權限判斷 預設判斷群組id
 function findPowerFromConstant(msg, temp) {
-    let a = messageConstant.Power.find(item => item.ChannelID == BDB.MGetChannelId(msg) && item.Power.indexOf(temp) != -1);
-    if (a !== undefined) temp = -1;
-    else if (messageConstant.Power.find(item => item.ChannelID == BDB.MGetChannelId(msg)) === undefined) {
-        a = messageConstant.Power.find(item => item.GroupID == BDB.MGetGuildId(msg) && item.Power.indexOf(temp) != -1);
-        if (a !== undefined) temp = -1;
-    }
-    return temp;
+	let a = messageConstant.Power.find(
+		(item) =>
+			item.ChannelID == BDB.MGetChannelId(msg) && item.Power.indexOf(temp) != -1
+	);
+	if (a !== undefined) temp = -1;
+	else if (
+		messageConstant.Power.find(
+			(item) => item.ChannelID == BDB.MGetChannelId(msg)
+		) === undefined
+	) {
+		a = messageConstant.Power.find(
+			(item) =>
+				item.GroupID == BDB.MGetGuildId(msg) && item.Power.indexOf(temp) != -1
+		);
+		if (a !== undefined) temp = -1;
+	}
+	return temp;
 }
